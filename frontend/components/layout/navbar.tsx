8e21839a039eef249ee181f5780c804e05ed1c2c
Kellen <webdevelopment@kellenwiltshire.com>
2022-06-30 19:40:49 -0400

    feat: adding Navbar code. Configuring

--- frontend/components/layout/navbar.tsx
+++ frontend/components/layout/navbar.tsx
@@ -1,9 +1,145 @@
-import React from 'react'
+import {Fragment} from 'react'
+import {Disclosure, Menu, Transition} from '@headlessui/react'
+import {SearchIcon} from '@heroicons/react/solid'
+import {BellIcon, MenuIcon, XIcon} from '@heroicons/react/outline'
 
-//Simple Navbar. Designed determined by Figma
-
-function Navbar(): JSX.Element {
-	return <nav></nav>
+function classNames(...classes) {
+	return classes.filter(Boolean).join(' ')
 }
 
-export default Navbar
+export default function Navbar() {
+	return (
+		<Disclosure as="nav" className="bg-white shadow">
+			{({open}) => (
+				<>
+					<div className="max-w-7xl mx-auto px-2 sm:px-4 lg:px-8">
+						<div className="flex justify-between h-16">
+							<div className="flex px-2 lg:px-0">
+								<div className="flex-shrink-0 flex items-center">
+									<img
+										className="block lg:hidden h-8 w-auto"
+										src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg"
+										alt="Workflow"
+									/>
+								</div>
+								<div className="hidden lg:ml-6 lg:flex lg:space-x-8">
+									{/* Current: "border-indigo-500 text-gray-900", Default: "border-transparent text-gray-500 hover:border-gray-300 hover:text-gray-700" */}
+									<a
+										href="#"
+										className="border-indigo-500 text-gray-900 inline-flex items-center px-1 pt-1 border-b-2 text-sm font-medium"
+									>
+										Reviews
+									</a>
+								</div>
+							</div>
+							<div className="flex-1 flex items-center justify-center px-2 lg:ml-6 lg:justify-end">
+								<div className="max-w-lg w-full lg:max-w-xs">
+									<label htmlFor="search" className="sr-only">
+										Search
+									</label>
+									<div className="relative">
+										<div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+											<SearchIcon
+												className="h-5 w-5 text-gray-400"
+												aria-hidden="true"
+											/>
+										</div>
+										<input
+											id="search"
+											name="search"
+											className="block w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md leading-5 bg-white placeholder-gray-500 focus:outline-none focus:placeholder-gray-400 focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+											placeholder="Search"
+											type="search"
+										/>
+									</div>
+								</div>
+							</div>
+							<div className="flex items-center lg:hidden">
+								{/* Mobile menu button */}
+								<Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-indigo-500">
+									<span className="sr-only">Open main menu</span>
+									{open ? (
+										<XIcon className="block h-6 w-6" aria-hidden="true" />
+									) : (
+										<MenuIcon className="block h-6 w-6" aria-hidden="true" />
+									)}
+								</Disclosure.Button>
+							</div>
+							<div className="hidden lg:ml-4 lg:flex lg:items-center">
+								<button
+									type="button"
+									className="flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+								>
+									<span className="sr-only">View notifications</span>
+									<BellIcon className="h-6 w-6" aria-hidden="true" />
+								</button>
+							</div>
+						</div>
+					</div>
+
+					<Disclosure.Panel className="lg:hidden">
+						<div className="pt-2 pb-3 space-y-1">
+							{/* Current: "bg-indigo-50 border-indigo-500 text-indigo-700", Default: "border-transparent text-gray-600 hover:bg-gray-50 hover:border-gray-300 hover:text-gray-800" */}
+							<Disclosure.Button
+								as="a"
+								href="#"
+								className="bg-indigo-50 border-indigo-500 text-indigo-700 block pl-3 pr-4 py-2 border-l-4 text-base font-medium"
+							>
+								Reviews
+							</Disclosure.Button>
+						</div>
+						<div className="pt-4 pb-3 border-t border-gray-200">
+							<div className="flex items-center px-4">
+								<div className="flex-shrink-0">
+									<img
+										className="h-10 w-10 rounded-full"
+										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+										alt=""
+									/>
+								</div>
+								<div className="ml-3">
+									<div className="text-base font-medium text-gray-800">
+										Tom Cook
+									</div>
+									<div className="text-sm font-medium text-gray-500">
+										tom@example.com
+									</div>
+								</div>
+								<button
+									type="button"
+									className="ml-auto flex-shrink-0 bg-white p-1 text-gray-400 rounded-full hover:text-gray-500 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+								>
+									<span className="sr-only">View notifications</span>
+									<BellIcon className="h-6 w-6" aria-hidden="true" />
+								</button>
+							</div>
+							<div className="mt-3 space-y-1">
+								<Disclosure.Button
+									as="a"
+									href="#"
+									className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+								>
+									Your Profile
+								</Disclosure.Button>
+								<Disclosure.Button
+									as="a"
+									href="#"
+									className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+								>
+									Settings
+								</Disclosure.Button>
+								<Disclosure.Button
+									as="a"
+									href="#"
+									className="block px-4 py-2 text-base font-medium text-gray-500 hover:text-gray-800 hover:bg-gray-100"
+								>
+									Sign out
+								</Disclosure.Button>
+							</div>
+						</div>
+					</Disclosure.Panel>
+				</>
+			)}
+		</Disclosure>
+	)
+}
