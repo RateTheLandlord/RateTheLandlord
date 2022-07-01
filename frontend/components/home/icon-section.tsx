48e9b947422522fca5933117867e3747f3efe48c
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-07-01 11:26:30 -0400

    feat: initial icon-section completed. waiting on correct icons

--- frontend/components/home/icon-section.tsx
+++ frontend/components/home/icon-section.tsx
@@ -1,11 +1,11 @@
 import React from 'react'
-import Privacy from '../svg/privacy'
+import Privacy from '../svg/icons/privacy'
 
 export default function IconSection() {
 	return (
 		<div className="bg-white min-w-full">
 			<div className="max-w-7xl mx-auto px-4 py-24 sm:px-6 lg:px-8">
-				<div className="bg-gray-50 rounded-2xl px-6 py-16 sm:p-16">
+				<div className="bg-gray-50 rounded-3xl px-6 py-16 sm:p-16">
 					<div className="max-w-xl mx-auto lg:max-w-none">
 						<div className="text-center">
 							<h2 className="text-2xl font-extrabold tracking-tight text-gray-900">
@@ -20,10 +20,10 @@ export default function IconSection() {
 									</div>
 								</div>
 								<div className="mt-3 sm:mt-0 sm:ml-6 lg:mt-6 lg:ml-0">
-									<h3 className="text-sm font-medium text-gray-900">
+									<h3 className="text-2xl font-extrabold text-gray-900">
 										Anonymity
 									</h3>
-									<p className="mt-2 text-sm text-gray-500">
+									<p className="mt-2 text-sm text-gray-900">
 										Share your rental experience with confidence.
 									</p>
 								</div>
@@ -31,14 +31,14 @@ export default function IconSection() {
 							<div className="text-center sm:flex sm:text-left lg:block lg:text-center">
 								<div className="sm:flex-shrink-0">
 									<div className="flow-root">
-										<Privacy />
+										<Privacy styling='className="w-16 h-16 mx-auto text-teal-600' />
 									</div>
 								</div>
 								<div className="mt-3 sm:mt-0 sm:ml-6 lg:mt-6 lg:ml-0">
-									<h3 className="text-sm font-medium text-gray-900">
+									<h3 className="text-2xl font-extrabold text-gray-900">
 										Solidarity
 									</h3>
-									<p className="mt-2 text-sm text-gray-500">
+									<p className="mt-2 text-sm text-gray-900">
 										Join fellow Tenants by creating an informed community.
 									</p>
 								</div>
@@ -46,14 +46,14 @@ export default function IconSection() {
 							<div className="text-center sm:flex sm:text-left lg:block lg:text-center">
 								<div className="sm:flex-shrink-0">
 									<div className="flow-root">
-										<Privacy />
+										<Privacy styling='className="w-16 h-16 mx-auto text-teal-600' />
 									</div>
 								</div>
 								<div className="mt-3 sm:mt-0 sm:ml-6 lg:mt-6 lg:ml-0">
-									<h3 className="text-sm font-medium text-gray-900">
+									<h3 className="text-2xl font-extrabold text-gray-900">
 										Transparency
 									</h3>
-									<p className="mt-2 text-sm text-gray-500">
+									<p className="mt-2 text-sm text-gray-900">
 										Empower others to make decisions about housing.
 									</p>
 								</div>
