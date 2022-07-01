bc4c510903ea9212a4f79bd96eae5d7f461a2a8d
Kellen <webdevelopment@kellenwiltshire.com>
2022-06-30 20:03:10 -0400

    fix: Tailind Importing

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,11 +1,16 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: [
-		'./pages/**/*.{js,ts,jsx,tsx}',
+		'./pages/**/*.{js, ts, jsx, tsx}',
 		'./components/**/*.{js,ts,jsx,tsx}',
 	],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [
+		require('@tailwindcss/typography'),
+		require('@tailwindcss/forms'),
+		require('@tailwindcss/line-clamp'),
+		require('@tailwindcss/aspect-ratio'),
+	],
 }
