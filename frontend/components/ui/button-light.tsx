5934fe1501772e48e27ce9cafc1fbfbba7b270fc
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-07-04 08:01:46 -0400

    feat: created default buttons

---
+++ frontend/components/ui/button-light.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+//Default Button. Design determined by Figma
+function ButtonLight({children}: {children: string}): JSX.Element {
+	return (
+		<button
+			type="submit"
+			// onClick={onClick}
+			className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+		>
+			{children}
+		</button>
+	)
+}
+
+export default ButtonLight
--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -2,8 +2,16 @@ import React from 'react'
 
 //Default Button. Design determined by Figma
 
-function Button(): JSX.Element {
-	return <div></div>
+function Button({children}: {children: string}): JSX.Element {
+	return (
+		<button
+			type="submit"
+			// onClick={onClick}
+			className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+		>
+			{children}
+		</button>
+	)
 }
 
 export default Button
