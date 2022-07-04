d997f7f0ba100f5816fdb4d2b62833d148179133
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-04 13:03:22 -0400

    fix: remove onClick from buttons

--- frontend/components/ui/button-light.tsx
+++ frontend/components/ui/button-light.tsx
@@ -4,8 +4,7 @@ import React from 'react'
 function ButtonLight({children}: {children: string}): JSX.Element {
 	return (
 		<button
-			type="submit"
-			// onClick={onClick}
+			type="reset"
 			className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
 		>
 			{children}
--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -6,7 +6,6 @@ function Button({children}: {children: string}): JSX.Element {
 	return (
 		<button
 			type="submit"
-			// onClick={onClick}
 			className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
 		>
 			{children}
