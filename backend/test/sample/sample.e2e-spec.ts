552c72805869c8181f377275bdc403f155fe6135
Zeshan Aslam <zeshan12aslam@gmail.com>
2022-07-03 23:32:41 -0400

    6: Test

--- backend/test/sample/sample.e2e-spec.ts
+++ backend/test/sample/sample.e2e-spec.ts
@@ -1,9 +1,9 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { INestApplication } from '@nestjs/common';
-import * as request from 'supertest';
-import { AppModule } from './../src/app.module';
+import request from 'supertest';
+import { AppModule } from 'src/app.module';
 
-describe('AppController (e2e)', () => {
+describe('SampleController (e2e)', () => {
   let app: INestApplication;
 
   beforeEach(async () => {
