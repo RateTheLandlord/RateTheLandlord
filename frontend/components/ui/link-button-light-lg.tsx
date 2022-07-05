08b9ec6f1b9b689695696d30eb880fa9b0cfc39d
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-05 19:16:58 -0400

    feat: added internal link buttons

---
+++ frontend/components/ui/link-button-light-lg.tsx
@@ -0,0 +1,20 @@
+import Link from 'next/link'
+import React from 'react'
+
+export default function LinkButtonLightLG({
+	children,
+	href,
+}: {
+	children: string
+	href: string
+}) {
+	return (
+		<div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3">
+			<Link href={href}>
+				<a className="w-full flex items-center justify-center px-8 py-3 border border-teal-600 text-base font-medium rounded-md text-teal-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
+					{children}
+				</a>
+			</Link>
+		</div>
+	)
+}
