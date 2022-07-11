bcde991a7d7e372fbaca513e13c7749da0bbe85d
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-11 09:10:58 -0400

    feat: added middleware to control localization

---
+++ frontend/pages/middleware.ts
@@ -0,0 +1,17 @@
+import {NextRequest, NextResponse} from 'next/server'
+
+const PUBLIC_FILE = /\.(.*)$/
+
+export async function middleware(req: NextRequest) {
+	if (
+		req.nextUrl.pathname.startsWith('/_next') ||
+		req.nextUrl.pathname.includes('/api/') ||
+		PUBLIC_FILE.test(req.nextUrl.pathname)
+	) {
+		return
+	}
+
+	if (req.nextUrl.locale === 'en-CA') {
+		return NextResponse.redirect(new URL(`/en${req.nextUrl.pathname}`, req.url))
+	}
+}
