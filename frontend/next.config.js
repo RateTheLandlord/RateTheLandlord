d501fd5927a4f578258c3d5ec8f7131443e94ca9
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-11 18:55:05 -0400

    feat: vonfig updated

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
 	reactStrictMode: true,
 	i18n: {
-		locales: ['en-CA', 'en-US', 'fr'],
+		locales: ['en-CA', 'fr'],
 		defaultLocale: 'en-CA',
 		localeDetection: true,
 		domains: [
