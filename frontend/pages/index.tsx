dc5a040f0c37b321c11d8606e42bd7f780f0599c
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-11 15:54:57 -0400

    feat: added layout and Static Props for Locale

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,14 +1,24 @@
 import Hero from '@/components/home/hero'
 import IconSection from '@/components/home/icon-section'
+import Layout from '@/components/layout/layout'
+import {GetStaticPropsContext} from 'next'
 import React from 'react'
 
 //This page should be statically generated at build. No need for Data fetching here
 
 export default function Home(): JSX.Element {
 	return (
-		<div>
+		<Layout>
 			<Hero />
 			<IconSection />
-		</div>
+		</Layout>
 	)
+}
+
+export async function getStaticProps({locale}: GetStaticPropsContext) {
+	return {
+		props: {
+			messages: (await import(`../localization/${locale}.json`)).default,
+		},
+	}
 }
