bc466ee2d3d2c2e4740f39497fd96f72a668e3b2
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-11 09:10:43 -0400

    feat: updated next-config for Localization

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,16 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  reactStrictMode: true,
-}
 
-module.exports = nextConfig
+module.exports = {
+	reactStrictMode: true,
+	i18n: {
+		locales: ['en-CA', 'en-US'],
+		defaultLocale: 'en-CA',
+		localeDetection: true,
+		domains: [
+			{
+				domain: 'ratethelandlord.org',
+				defaultLocale: 'en-CA',
+			},
+		],
+	},
+}
