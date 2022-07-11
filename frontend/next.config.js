83c842d79330f5dbdbf6cd33d6736fe02635016e
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-11 11:58:30 -0400

    feat: update next config for localization

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,7 @@
 module.exports = {
 	reactStrictMode: true,
 	i18n: {
-		locales: ['en-CA', 'en-US'],
+		locales: ['en-CA', 'en-US', 'fr'],
 		defaultLocale: 'en-CA',
 		localeDetection: true,
 		domains: [
