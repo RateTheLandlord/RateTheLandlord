770dd5a18db42f79a9073fe7d06ce0cb582e656d
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-11 11:58:42 -0400

    feat: added locale check

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,10 +1,15 @@
 import Layout from '@/components/layout/layout'
 import {AppProps} from 'next/app'
+import {useRouter} from 'next/router'
 import '../styles/global.css'
 
 //State for Admin Login may be held here (Admin Status {Logged In? Username?})
 
 function MyApp({Component, pageProps}: AppProps): JSX.Element {
+	//This gets the current Locale. This can be passed down to pages/components or we can update the _app.tsx to show different info based on locale
+	//Translation will need to be done for French, other languages as needed.
+	const router = useRouter()
+	console.log(router.locale)
 	return (
 		<Layout>
 			<Component {...pageProps} />
