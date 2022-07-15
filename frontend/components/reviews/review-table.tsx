6688d74f929f73413616739d7c45a5db289d6c85
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-15 08:58:56 -0400

    feat: data fetch implemented

--- frontend/components/reviews/review-table.tsx
+++ frontend/components/reviews/review-table.tsx
@@ -2,7 +2,8 @@ import React from 'react'
 
 //Review table. Data will need to be shared with Review Filters. Data received from Review Page
 
-function ReviewTable(): JSX.Element {
+function ReviewTable({data}): JSX.Element {
+	console.log(data)
 	return <div></div>
 }
 
--- frontend/pages/reviews.tsx
+++ frontend/pages/reviews.tsx
@@ -1,19 +1,34 @@
 import ReviewFilters from '@/components/reviews/review-filters'
 import ReviewTable from '@/components/reviews/review-table'
 import React from 'react'
+import useSWR, {SWRConfig} from 'swr'
 
 //This page should be statically generated for better SEO performance with getStaticProps through NextJS with a short regeneration period (30 minute?)
 //Once the page is loaded, though, SWR should be used to get up-to-date reviews
 //Data passed to Review Filters and Review Table
 
-function Reviews(): JSX.Element {
+const fetcher = (url) => fetch(url).then((r) => r.json())
+
+export default function Reviews({fallback}): JSX.Element {
+	const {data} = useSWR('/api/get-reviews', fetcher)
 	return (
-		<div>
+		<SWRConfig value={{fallback}}>
 			Reviews
 			<ReviewFilters />
-			<ReviewTable />
-		</div>
+			<ReviewTable data={data} />
+		</SWRConfig>
 	)
 }
 
-export default Reviews
+export async function getStaticProps() {
+	// `getStaticProps` is executed on the server side.
+	const article = await fetch('/api/get-reviews')
+	const data = await article.json()
+	return {
+		props: {
+			fallback: {
+				'/api/get-reviews': data,
+			},
+		},
+	}
+}
