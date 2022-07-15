68a17cdc4f861129127dabf99a439d4bed12df59
Kellen <webdevelopment@kellenwiltshire.com>
2022-07-15 09:38:02 -0400

    feat: added interfaces file

---
+++ frontend/util/interfaces.ts
@@ -0,0 +1,14 @@
+export interface Data {
+	city: string
+	countrycode: string
+	health: string
+	id: number
+	landlord: string
+	privacy: string
+	repair: string
+	respect: string
+	review: string
+	stability: string
+	state: string
+	zip: string
+}
