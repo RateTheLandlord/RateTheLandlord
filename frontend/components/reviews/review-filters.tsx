c134f52a7c1c9c3b797f741605f4ca371a7a46ac
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-07-17 11:56:40 -0400

    feat: implemented sorting reviews alphabetically

--- frontend/components/reviews/review-filters.tsx
+++ frontend/components/reviews/review-filters.tsx
@@ -1,24 +1,35 @@
 import React, {useState, Fragment} from 'react'
-import {Dialog, Disclosure, Menu, Popover, Transition} from '@headlessui/react'
+import {
+	Dialog,
+	Disclosure,
+	Listbox,
+	Popover,
+	Transition,
+} from '@headlessui/react'
 import {XIcon} from '@heroicons/react/outline'
 import {classNames} from '@/util/classnames'
-import {ChevronDownIcon} from '@heroicons/react/solid'
+import {CheckIcon, ChevronDownIcon, SelectorIcon} from '@heroicons/react/solid'
 import {ActiveFilters, Filters, SortOptions} from '@/util/interfaces'
 
 //Review filters and Logic
 
-interface Props {
+interface FiltersProps {
 	filters: [Filters]
-	sortOptions: [SortOptions]
 	activeFilters: [ActiveFilters]
+	selectedSort: SortOptions
+	setSelectedSort: (selectedSort: SortOptions) => void
+	sortOptions: [SortOptions]
 }
 
 function ReviewFilters({
 	filters,
-	sortOptions,
 	activeFilters,
-}: Props): JSX.Element {
+	selectedSort,
+	setSelectedSort,
+	sortOptions,
+}: FiltersProps): JSX.Element {
 	const [mobileFiltersOpen, setMobileFiltersOpen] = useState<boolean>(false)
+
 	return (
 		<div>
 			{/* Mobile filter dialog */}
@@ -146,51 +157,59 @@ function ReviewFilters({
 
 					<div className="relative z-10 bg-white border-b border-gray-200 pb-4">
 						<div className="max-w-7xl mx-auto px-4 flex items-center justify-between sm:px-6 lg:px-8">
-							<Menu as="div" className="relative inline-block text-left">
-								<div>
-									<Menu.Button className="group inline-flex justify-center text-sm font-medium text-gray-700 hover:text-gray-900">
-										Sort
-										<ChevronDownIcon
-											className="flex-shrink-0 -mr-1 ml-1 h-5 w-5 text-gray-400 group-hover:text-gray-500"
-											aria-hidden="true"
-										/>
-									</Menu.Button>
-								</div>
-
-								<Transition
-									as={Fragment}
-									enter="transition ease-out duration-100"
-									enterFrom="transform opacity-0 scale-95"
-									enterTo="transform opacity-100 scale-100"
-									leave="transition ease-in duration-75"
-									leaveFrom="transform opacity-100 scale-100"
-									leaveTo="transform opacity-0 scale-95"
-								>
-									<Menu.Items className="origin-top-left absolute left-0 mt-2 w-40 rounded-md shadow-2xl bg-white ring-1 ring-black ring-opacity-5 focus:outline-none">
-										<div className="py-1">
+							<Listbox value={selectedSort} onChange={setSelectedSort}>
+								<div className="relative mt-1">
+									<Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
+										<span className="block truncate">Sorting Options</span>
+										<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
+											<SelectorIcon
+												className="h-5 w-5 text-gray-400"
+												aria-hidden="true"
+											/>
+										</span>
+									</Listbox.Button>
+									<Transition
+										as={Fragment}
+										leave="transition ease-in duration-100"
+										leaveFrom="opacity-100"
+										leaveTo="opacity-0"
+									>
+										<Listbox.Options className="absolute mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
 											{sortOptions.map((option) => (
-												<Menu.Item key={option.name}>
-													{({active}) => (
-														<a
-															href={option.href}
-															className={classNames(
-																option.current
-																	? 'font-medium text-gray-900'
-																	: 'text-gray-500',
-																active ? 'bg-gray-100' : '',
-																'block px-4 py-2 text-sm',
-															)}
-														>
-															{option.name}
-														</a>
-													)}
-												</Menu.Item>
+												<Listbox.Option
+													key={option.id}
+													className={({active}) =>
+														`relative cursor-default select-none py-2 pl-10 pr-4 ${
+															active
+																? 'bg-amber-100 text-amber-900'
+																: 'text-gray-900'
+														}`
+													}
+													value={option}
+												>
+													<span
+														className={`block truncate ${
+															option.name === selectedSort.name
+																? 'font-medium'
+																: 'font-normal'
+														}`}
+													>
+														{option.name}
+													</span>
+													{option.name === selectedSort.name ? (
+														<span className="absolute inset-y-0 left-0 flex items-center pl-3 text-amber-600">
+															<CheckIcon
+																className="h-5 w-5"
+																aria-hidden="true"
+															/>
+														</span>
+													) : null}
+												</Listbox.Option>
 											))}
-										</div>
-									</Menu.Items>
-								</Transition>
-							</Menu>
-
+										</Listbox.Options>
+									</Transition>
+								</div>
+							</Listbox>
 							<button
 								type="button"
 								className="inline-block text-sm font-medium text-gray-700 hover:text-gray-900 sm:hidden"
--- frontend/pages/reviews.tsx
+++ frontend/pages/reviews.tsx
@@ -1,22 +1,46 @@
 import ReviewFilters from '@/components/reviews/review-filters'
 import ReviewTable from '@/components/reviews/review-table'
-import {AllReviews, Review} from '@/util/interfaces'
+import {AllReviews, Review, SortOptions} from '@/util/interfaces'
+import {sortAZ, sortZA} from '@/util/sort-filters'
 
-import React from 'react'
+import React, {useEffect, useState} from 'react'
 import useSWR, {SWRConfig} from 'swr'
 
 //fallback is the data from getStaticProps. It is used as the initial data for building the page. This data is then checked against the data received from useSWR and will be updated accordingly
 
 const fetcher = (url: string) => fetch(url).then((r) => r.json())
 
+const sortOptions = [
+	{id: 1, name: 'Name A-Z'},
+	{id: 2, name: 'Name Z-A'},
+]
+
 export default function Reviews({
 	fallback,
 }: {
 	fallback: [AllReviews]
 }): JSX.Element {
+	const [selectedSort, setSelectedSort] = useState<SortOptions>(sortOptions[0])
 	const initialData = fallback['/api/get-reviews'] as [Review]
 	const {data} = useSWR<[Review]>('/api/get-reviews', fetcher)
-	console.log('fallback: ', fallback)
+
+	const [reviews, setReviews] = useState<[Review]>(initialData)
+
+	useEffect(() => {
+		if (selectedSort.name === 'Name A-Z') {
+			const result = sortAZ(reviews)
+			setReviews(result)
+		} else {
+			const result = sortZA(reviews)
+			setReviews(result)
+		}
+	}, [reviews, selectedSort])
+
+	useEffect(() => {
+		if (data) {
+			setReviews(data)
+		}
+	}, [data])
 
 	const filters = [
 		{
@@ -44,16 +68,15 @@ export default function Reviews({
 			],
 		},
 	]
-	const sortOptions = [
-		{name: 'Name A-Z', href: '#', current: true},
-		{name: 'Name Z-A', href: '#', current: false},
-	]
+
 	const activeFilters = [{value: 'canada', label: 'Canada'}]
 	return (
 		<SWRConfig value={{fallback}}>
 			<div>
 				<ReviewFilters
 					filters={filters}
+					selectedSort={selectedSort}
+					setSelectedSort={setSelectedSort}
 					sortOptions={sortOptions}
 					activeFilters={activeFilters}
 				/>
@@ -67,7 +90,7 @@ export default function Reviews({
 export async function getStaticProps() {
 	//URL will need to be updated to match site URL
 	const article = await fetch('http://localhost:3000/api/get-reviews')
-	const data = (await article.json()) as [Data]
+	const data = (await article.json()) as [Review]
 	return {
 		props: {
 			fallback: {
