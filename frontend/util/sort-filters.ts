aadb6967143fdf080a379c4f145b166854934218
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-07-17 11:56:26 -0400

    feat: created util file to store sorting and filtering functions

---
+++ frontend/util/sort-filters.ts
@@ -0,0 +1,18 @@
+import {AllReviews, Review} from './interfaces'
+
+export const sortAZ = (data: [Review]): [Review] => {
+	console.log('data: ', data)
+	const result = data.sort((a: Review, b: Review) =>
+		b.landlord.localeCompare(a.landlord),
+	)
+
+	return result
+}
+
+export const sortZA = (data: [Review]): [Review] => {
+	const result = data.sort((a: Review, b: Review) =>
+		a.landlord.localeCompare(b.landlord),
+	)
+
+	return result
+}
