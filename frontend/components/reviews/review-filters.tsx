69fb8684f1f39485d05465b2c09bbf1d56fb8537
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-07-18 07:54:05 -0400

    fix: sort box name

--- frontend/components/reviews/review-filters.tsx
+++ frontend/components/reviews/review-filters.tsx
@@ -160,7 +160,9 @@ function ReviewFilters({
 							<Listbox value={selectedSort} onChange={setSelectedSort}>
 								<div className="relative mt-1">
 									<Listbox.Button className="relative w-full cursor-default rounded-lg bg-white py-2 pl-3 pr-10 text-left shadow-md focus:outline-none focus-visible:border-indigo-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 focus-visible:ring-offset-orange-300 sm:text-sm">
-										<span className="block truncate">Sorting Options</span>
+										<span className="block truncate w-20">
+											{selectedSort.name}
+										</span>
 										<span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
 											<SelectorIcon
 												className="h-5 w-5 text-gray-400"
