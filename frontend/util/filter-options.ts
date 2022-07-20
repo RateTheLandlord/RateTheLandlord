d161800031de3510ff3e644ce586e34737a62971
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-07-20 16:22:07 -0400

    fix: type

--- frontend/util/filter-options.ts
+++ frontend/util/filter-options.ts
@@ -1,17 +1,11 @@
-import countries from '@/util/countries.json'
-
 export const sortOptions = [
 	{id: 1, name: 'Name A-Z', value: 'az'},
 	{id: 2, name: 'Name Z-A', value: 'za'},
 ]
 
-export const countryOptions = Object.keys(countries)
-	.filter((c) => c === 'CA' || c === 'US')
-	.forEach((obj, ind) => (obj.id = ind + 1))
-
 export const stateOptions = [{id: 1, name: 'Ontario', value: 'ON'}]
 
 export const cityOptions = [
-	{id: 1, name: 'Mississauga', value: 'MISS'},
-	{id: 2, Name: 'Toronto', value: 'TOR'},
+	{id: 1, name: 'Mississauga', value: 'Mississauga'},
+	{id: 2, name: 'Toronto', value: 'Toronto'},
 ]
