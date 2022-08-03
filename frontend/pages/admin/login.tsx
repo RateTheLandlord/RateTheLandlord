fe09a5d8058882edcfbe8b8a07052ecf573727b0
Kellen Wiltshire <kellenwiltshire@outlook.com>
2022-08-03 07:44:48 -0400

    feat: login created

--- frontend/pages/admin/login.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-function Login(): JSX.Element {
-	return <div>Admin Login</div>
-}
-
-export default Login
---
+++ frontend/pages/login.tsx
@@ -0,0 +1,12 @@
+import LoginForm from '@/components/login/login-form'
+import React from 'react'
+
+function Login(): JSX.Element {
+	return (
+		<div>
+			<LoginForm />
+		</div>
+	)
+}
+
+export default Login
