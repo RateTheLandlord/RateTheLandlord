11df86405e89030640b88ceb9de9f1c933a78855
kellenwiltshire <kellenwiltshire@outlook.com>
2023-02-11 16:07:34 -0500

    Initial About Page Created

---
+++ frontend/components/about/contact.tsx
@@ -0,0 +1,14 @@
+const Contact = () => {
+	return (
+		<div>
+			<h3 className="text-2xl font-bold text-center">Contact Us</h3>
+			<div className="text-center">
+				<a href="mailto:r8thelandlord@gmail.com">
+					Email us: r8thelandlord@gmail.com
+				</a>
+			</div>
+		</div>
+	)
+}
+
+export default Contact
---
+++ frontend/components/about/moderation.tsx
@@ -0,0 +1,21 @@
+const Moderation = () => {
+	return (
+		<div className="mx-auto max-w-prose text-lg">
+			<h3>
+				<span className="mt-2 block text-center text-xl font-bold leading-8 tracking-tight text-gray-900 sm:text-2xl">
+					Moderation Policy
+				</span>
+			</h3>
+			<p className="mt-8 text-xl leading-8 text-gray-500">
+				We allow our users to share their opinions and experiences through
+				reviews, but we will not tolerate excessive profanity or personal
+				attacks. If a review contains excessive profanity or personal attacks,
+				it may be censored to remove the offensive language. Additionally, we
+				will remove any reviews that contain defamatory remarks or
+				discrimination.
+			</p>
+		</div>
+	)
+}
+
+export default Moderation
