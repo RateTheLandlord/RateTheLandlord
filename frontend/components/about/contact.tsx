de57702381c4fcc5b43520b4672abeb58d04c5d8
kellenwiltshire <kellenwiltshire@outlook.com>
2023-02-12 11:16:11 -0500

    Match Contact us to style of About

--- frontend/components/about/contact.tsx
+++ frontend/components/about/contact.tsx
@@ -1,9 +1,16 @@
 const Contact = () => {
 	return (
 		<div>
-			<h3 className="text-2xl font-bold text-center">Contact Us</h3>
+			<h3>
+				<span className="mt-2 block text-center text-xl font-bold leading-8 tracking-tight text-gray-900 sm:text-2xl">
+					Contact Us
+				</span>
+			</h3>
 			<div className="text-center">
-				<a href="mailto:r8thelandlord@gmail.com">
+				<a
+					href="mailto:r8thelandlord@gmail.com"
+					className="mt-8 text-xl leading-8 text-gray-500"
+				>
 					Email us: r8thelandlord@gmail.com
 				</a>
 			</div>
