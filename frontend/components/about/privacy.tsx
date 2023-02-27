d4b7749e4a9d186ef90f2f01efb4ac6d569b977f
kellenwiltshire <kellenwiltshire@outlook.com>
2023-02-26 19:30:12 -0500

    Polishing

--- frontend/components/about/privacy.tsx
+++ frontend/components/about/privacy.tsx
@@ -1,18 +1,20 @@
 const Privacy = () => {
 	return (
-		<div className="mx-auto max-w-prose text-lg">
-			<h3>
-				<span className="mt-2 block text-center text-xl font-bold leading-8 tracking-tight text-gray-900 sm:text-2xl">
-					Privacy Policy
-				</span>
-			</h3>
-			<p className="mt-8 text-xl leading-8 text-gray-500">
-				At Rate the Landlord, we are committed to protecting the privacy of
-				those who submit reviews. All submissions made to our website are
-				anonymous. We do not collect any personal data or information that could
-				be used to identify you, such as your name, email address, or phone
-				number.
-			</p>
+		<div className="bg-white w-full">
+			<div className="mx-auto max-w-7xl px-6 lg:px-8 text-lg">
+				<h3>
+					<span className="mt-2 block text-center text-xl font-bold leading-8 tracking-tight text-gray-900 sm:text-2xl">
+						Privacy Policy
+					</span>
+				</h3>
+				<p className="mt-8 text-xl leading-8 text-gray-500">
+					At Rate the Landlord, we are committed to protecting the privacy of
+					those who submit reviews. All submissions made to our website are
+					anonymous. We do not collect any personal data or information that
+					could be used to identify you, such as your name, email address, or
+					phone number.
+				</p>
+			</div>
 		</div>
 	)
 }
