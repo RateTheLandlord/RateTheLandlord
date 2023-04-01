f08fe7aff23a63e43f6fdd560c4126de3441b684
kellenwiltshire <kellenwiltshire@outlook.com>
2023-03-31 20:08:49 -0400

    fix

--- frontend/pages/reviews.tsx
+++ frontend/pages/reviews.tsx
@@ -14,16 +14,27 @@ export default function Reviews({fallback}: {fallback: Review[]}): JSX.Element {
 
 //Page is statically generated at build time and then revalidated at a minimum of every 100 seconds based on when the page is accessed
 export async function getStaticProps() {
-	const req = await fetch(`http://backend:5000/review`)
-	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-	const data: Review[] = await req.json()
+	try {
+		const req = await fetch(`http://backend:5000/review`)
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+		const data: Review[] = await req.json()
 
-	return {
-		props: {
-			fallback: {
-				'/api/get-reviews': data ? data : [],
+		return {
+			props: {
+				fallback: {
+					'/api/get-reviews': data ? data : [],
+				},
 			},
-		},
-		revalidate: 100,
+			revalidate: 100,
+		}
+	} catch (error) {
+		return {
+			props: {
+				fallback: {
+					'/api/get-reviews': [],
+				},
+			},
+			revalidate: 100,
+		}
 	}
 }
