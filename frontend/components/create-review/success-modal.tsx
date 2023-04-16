fb5313dad0e3b62e535bdf28b2a43854830286c1
kellenwiltshire <kellenwiltshire@outlook.com>
2023-04-16 12:13:11 -0400

    131: Update Success Modal and Improve disable look of button

--- frontend/components/create-review/success-modal.tsx
+++ frontend/components/create-review/success-modal.tsx
@@ -1,5 +1,5 @@
-import React, {SetStateAction} from 'react'
-import {Dialog} from '@headlessui/react'
+import React, {SetStateAction, Fragment} from 'react'
+import {Dialog, Transition} from '@headlessui/react'
 import ButtonLight from '../ui/button-light'
 import Button from '../ui/button'
 import {useRouter} from 'next/router'
@@ -14,42 +14,71 @@ function SuccessModal({isOpen, setIsOpen}: IProps) {
 	const {t} = useTranslation('create')
 	const router = useRouter()
 	return (
-		<Dialog
-			className="relative z-50"
-			open={isOpen}
-			onClose={() => setIsOpen(false)}
-		>
-			<div className="fixed inset-0 bg-black/30" aria-hidden="true" data-testid="success-modal-1" />
-			<div className="fixed inset-0 flex flex-col items-center justify-center p-4">
-				<Dialog.Panel className="w-full max-w-sm rounded-md bg-white p-10 flex flex-col">
-					<Dialog.Title className="mb-4 w-full flex justify-center">
-						{t('create-review.modal.success')}
-					</Dialog.Title>
-					<p className="text-center my-3">
-						{t('create-review.modal.description')}
-					</p>
+		<Transition.Root show={isOpen} as={Fragment}>
+			<Dialog as="div" className="relative z-10" onClose={setIsOpen}>
+				<Transition.Child
+					as={Fragment}
+					enter="ease-out duration-300"
+					enterFrom="opacity-0"
+					enterTo="opacity-100"
+					leave="ease-in duration-200"
+					leaveFrom="opacity-100"
+					leaveTo="opacity-0"
+				>
+					<div className="fixed inset-0 bg-gray-500 bg-opacity-75 transition-opacity" />
+				</Transition.Child>
 
-					<div className="flex flex-row gap-2" data-testid="success-modal-2">
-						<ButtonLight
-							onClick={() => {
-								setIsOpen(false)
-								router.reload()
-							}}
+				<div className="fixed inset-0 z-10 overflow-y-auto">
+					<div className="flex min-h-full items-end justify-center p-4 text-center sm:items-center sm:p-0">
+						<Transition.Child
+							as={Fragment}
+							enter="ease-out duration-300"
+							enterFrom="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
+							enterTo="opacity-100 translate-y-0 sm:scale-100"
+							leave="ease-in duration-200"
+							leaveFrom="opacity-100 translate-y-0 sm:scale-100"
+							leaveTo="opacity-0 translate-y-4 sm:translate-y-0 sm:scale-95"
 						>
-							{t('create-review.modal.submit-another')}
-						</ButtonLight>
-						<Button
-							onClick={() => {
-								setIsOpen(false)
-								router.push('/reviews').catch((err) => console.log(err))
-							}}
-						>
-							{t('create-review.modal.go-to')}
-						</Button>
+							<Dialog.Panel className="relative transform overflow-hidden rounded-lg bg-white px-4 pb-4 pt-5 text-left shadow-xl transition-all sm:my-8 sm:w-full sm:max-w-lg sm:p-6">
+								<div>
+									<div className="mt-3 text-center sm:mt-5">
+										<Dialog.Title
+											as="h3"
+											className="text-base font-semibold leading-6 text-gray-900"
+										>
+											{t('create-review.modal.success')}
+										</Dialog.Title>
+										<div className="mt-2">
+											<p className="text-sm text-gray-500">
+												{t('create-review.modal.description')}
+											</p>
+										</div>
+									</div>
+								</div>
+								<div className="mt-5 sm:mt-6 sm:grid sm:grid-flow-row-dense sm:grid-cols-2 sm:gap-3">
+									<ButtonLight
+										onClick={() => {
+											setIsOpen(false)
+											router.reload()
+										}}
+									>
+										{t('create-review.modal.submit-another')}
+									</ButtonLight>
+									<Button
+										onClick={() => {
+											setIsOpen(false)
+											router.push('/reviews').catch((err) => console.log(err))
+										}}
+									>
+										{t('create-review.modal.go-to')}
+									</Button>
+								</div>
+							</Dialog.Panel>
+						</Transition.Child>
 					</div>
-				</Dialog.Panel>
-			</div>
-		</Dialog>
+				</div>
+			</Dialog>
+		</Transition.Root>
 	)
 }
 
--- frontend/components/ui/button.tsx
+++ frontend/components/ui/button.tsx
@@ -15,7 +15,9 @@ function Button({
 		<button
 			onClick={onClick}
 			type="submit"
-			className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+			className={`ml-3 inline-flex justify-center rounded-md border border-transparent py-2 px-4 text-sm font-medium text-white shadow-sm focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2 ${
+				disabled ? 'bg-teal-200' : 'bg-teal-600 hover:bg-teal-700'
+			}`}
 			disabled={disabled}
 			data-testid="submit-button-1"
 		>
