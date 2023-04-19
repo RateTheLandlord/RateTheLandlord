7262783c24c8284f8c3c439de7c1422e2d7fe2df
stephenmartin2951 <stephenmartin2951@pm.me>
2023-04-19 07:26:08 -0500

    #141
    Additional changes

--- frontend/util/helper-functions.ts
+++ frontend/util/helper-functions.ts
@@ -9,6 +9,7 @@ export const capitalize = (str: string) => {
 }
 export const removeDuplicates = (arr: Options[], key: string): Options[] => {
 	const check = new Set()
+	key.replace(/\s/g, "")
 	return arr.filter(
 		(obj) =>
 			!check.has(obj[key as keyof Options]) &&
