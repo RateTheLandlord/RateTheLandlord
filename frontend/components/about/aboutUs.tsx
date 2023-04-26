25b1ff5fe4bdecf69290e0fd53acc25eab46471d
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-04-26 07:59:58 -0400

    PL-168: Updated About Page

--- frontend/components/about/aboutUs.tsx
+++ frontend/components/about/aboutUs.tsx
@@ -3,14 +3,14 @@ import {useTranslation} from 'react-i18next'
 const AboutUs = () => {
 	const {t} = useTranslation('about')
 	return (
-		<div data-testid="about-aboutus-1" className="bg-white w-full">
-			<div className="mx-auto max-w-7xl px-6 lg:px-8 text-lg">
+		<div data-testid="about-aboutus-1" className="w-full bg-white">
+			<div className="mx-auto max-w-7xl px-6 text-lg lg:px-8">
 				<h1>
 					<span className="mt-2 block text-center text-3xl font-bold leading-8 tracking-tight text-gray-900 sm:text-4xl">
 						{t('about.about-us.about')}
 					</span>
 				</h1>
-				<p className="mt-8 text-xl leading-8 text-gray-500 text-center font-bold">
+				<p className="mt-8 text-center text-xl font-bold leading-8 text-gray-500">
 					{t('about.about-us.header')}
 				</p>
 				<p className="mt-8 text-xl leading-8 text-gray-500">
@@ -21,6 +21,9 @@ const AboutUs = () => {
 				</p>
 				<p className="mt-8 text-xl leading-8 text-gray-500">
 					{t('about.about-us.p-3')}
+				</p>
+				<p className="mt-8 text-xl leading-8 text-gray-500">
+					{t('about.about-us.p-4')}
 				</p>
 			</div>
 		</div>
