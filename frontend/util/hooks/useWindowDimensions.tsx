757c06ddc7a6ee284a415eeecdcb20dc252da3c6
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-04-27 07:50:28 -0400

    add window size hook

---
+++ frontend/util/hooks/useWindowDimensions.tsx
@@ -0,0 +1,33 @@
+import { useState, useEffect } from "react";
+
+
+const useWindowSize = () =>  {
+    const [windowSize, setWindowSize] = useState({
+      width: undefined,
+      height: undefined,
+    });
+  
+    useEffect(() => {
+      // Handler to call on window resize
+      function handleResize() {
+        // Set window width/height to state
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      }
+  
+      // Add event listener
+      window.addEventListener("resize", handleResize);
+  
+      // Call handler right away so state gets updated with initial window size
+      handleResize();
+  
+      // Remove event listener on cleanup
+      return () => window.removeEventListener("resize", handleResize);
+    }, []); // Empty array ensures that effect is only run on mount
+  
+    return windowSize;
+  }
+
+  export default useWindowSize
