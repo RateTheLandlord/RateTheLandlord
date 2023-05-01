92607dd92f4f21489aee52e65c4cc477f411136c
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-04-30 21:51:47 -0400

    Checking in

--- backend/src/review/helpers.ts
+++ backend/src/review/helpers.ts
@@ -1,33 +1,27 @@
 import { Configuration, OpenAIApi } from 'openai';
 
+import BadWordsFilter from 'bad-words';
 import { Review } from './models/review';
 
 const SYSTEM_MESSAGE = `
-    You are a review moderator. You are tasked with editing reviews.
+You are a review moderator. You are tasked with editing reviews.
     
-    This type of content is not allowed:
+This type of content is not allowed:
 
-    - Addresses 
-    - Phone numbers
-    - Emails
-    - Swear words - including "slumlord"
-    - Racial slurs
+- Home addresses
+- Phone numbers
+- Emails
+- Swear words
+- Racial slurs or content relating to race
 
-    Here's how interactions with the user will work:
+Here's how this will work
+- I will provide you with a single review, which may have multiple paragraphs, and may or may not contain content that is not allowed
+- You will keep the review as close as possible to the original words and format
+- You will use the rules above to replace all content that is not allowed with *********
+- You will reply ONLY with the edited review, no additional prose or commentary    
 
-    - They will provide you with a single review, which may have multiple paragraphs
-    - You will keep the review as close as possible to the original words and format
-    - You will use the rules above to replace any content that is not allowed with *********
-    - If there are no edits to be made, you will respond with the original review
-    - You will reply ONLY with the edited review, no additional prose or commentary
+Here is the review you need to edit (reminder, you can only edit the review, no additional prose or commentary): 
 
-    Here's an example of a review that needs to be edited:
-
-    I lived in this apartment at 177 Motly St for 2 years. The landlord was a total asshole. He was always late to fix things and he never returned my security deposit. I'd try calling him at 1233051666 and he never picks up. I would not recommend this apartment to anyone.
-
-    Here's the correct response:
-
-    I lived in this apartment at ********* for 2 years. The landlord was a total ********. He was always late to fix things and he never returned my security deposit. I'd try calling him at ******** and he never picks up. I would not recommend this apartment to anyone.
 `;
 
 const configuration = new Configuration({
@@ -35,7 +29,7 @@ const configuration = new Configuration({
 });
 const openai = new OpenAIApi(configuration);
 
-export const filterReview = async (review: Review): Promise<Review> => {
+export const filterReviewWithAI = async (review: Review): Promise<Review> => {
   const completion = await openai.createChatCompletion({
     model: 'gpt-3.5-turbo',
     messages: [
@@ -45,5 +39,31 @@ export const filterReview = async (review: Review): Promise<Review> => {
   });
 
   review.review = completion.data.choices[0].message.content ?? '';
+
+  return review;
+};
+
+const badWordsFilter = new BadWordsFilter();
+
+export const filterReview = (review: Review) => {
+  // Replace addresses
+  // This pattern is more permissive and covers a wider range of addresses, but may also have false positives
+  const addressPattern =
+    /(^|\s)\d+\s+\w+(\s+\w+)*(,\s*\w+(\s+\w+)*)*(\s+(Avenue|Street|Road|Boulevard|Drive|Terrace|Place|Court|Crescent|Lane|Parkway|Way|Circle|Heights|Loop|Alley|Run|Glen|Bend|Plaza|Trace|Row))?(\.)?(?=\s|$)/gi;
+
+  review.review = review.review.replace(addressPattern, '*****');
+
+  // Replace phone numbers
+  const phonePattern =
+    /(\+\d{1,4}[-.\s]?)?(\(?\d{1,4}\)?[-.\s]?)?(\d{1,4}[-.\s]?){2,4}\d{1,4}([-.\s]?(x|ext\.?|extension)\s?\d{1,6})?/gi;
+  review.review = review.review.replace(phonePattern, '*****');
+
+  // Replace emails
+  const emailPattern = /[\w\.-]+@[\w\.-]+\.\w+/gi;
+  review.review = review.review.replace(emailPattern, '*****');
+
+  // Replace swear words
+  review.review = badWordsFilter.clean(review.review);
+
   return review;
 };
