0402857a1eabd450597ed004de6ba3aa4ef64eb7
Rate The Landlord <107703532+RateTheLandlordAdmin@users.noreply.github.com>
2023-05-03 17:36:56 -0400

    224 adopt google review policy and amend site to reflect (#241)
    * First pass at new moderation policy
    
    * Final tweaks to language

--- frontend/components/about/moderation.tsx
+++ frontend/components/about/moderation.tsx
@@ -2,15 +2,26 @@ import {useTranslation} from 'react-i18next'
 
 const Moderation = () => {
 	const {t} = useTranslation('about')
+
+	const paragraphs = [
+		t('about.moderation.p-1'),
+		t('about.moderation.p-2'),
+		t('about.moderation.p-3'),
+		t('about.moderation.p-4'),
+	]
 	return (
-		<div data-testid="about-moderation-1" className="bg-white w-full">
-			<div className="mx-auto max-w-7xl px-6 lg:px-8 text-lg">
+		<div data-testid="about-moderation-1" className="w-full bg-white">
+			<div className="mx-auto max-w-7xl px-6 text-lg lg:px-8">
 				<h3 className="mt-2 block text-center text-xl font-bold leading-8 tracking-tight text-gray-900 sm:text-2xl">
 					{t('about.moderation.moderation')}
 				</h3>
-				<p className="mt-8 text-xl leading-8 text-gray-500">
-					{t('about.moderation.p-1')}
-				</p>
+				{paragraphs.map((p, i) => {
+					return (
+						<p key={i} className="mt-8 text-xl leading-8 text-gray-500">
+							{p}
+						</p>
+					)
+				})}
 			</div>
 		</div>
 	)
