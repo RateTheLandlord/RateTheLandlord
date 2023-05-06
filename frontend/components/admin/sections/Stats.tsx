2b4fcb08f35ee7f6562b8074ad215706a290f7f6
Rate The Landlord <107703532+RateTheLandlordAdmin@users.noreply.github.com>
2023-05-06 19:29:45 -0400

    127 add review stats to admin page (#250)
    * Working on Admin Stats
    
    * Refactoring
    
    * Refactoring

---
+++ frontend/components/admin/sections/Stats.tsx
@@ -0,0 +1,130 @@
+import useSWR from 'swr'
+import StateStats from '../components/StateStats'
+import {useState} from 'react'
+
+const fetcher = (url: string) => fetch(url).then((r) => r.json())
+
+export interface IStats {
+	total_reviews: number
+	total_ca_reviews: {
+		total: string
+		states: Array<{
+			key: string
+			total: string
+		}>
+	}
+	total_us_reviews: {
+		total: string
+		states: Array<{
+			key: string
+			total: string
+		}>
+	}
+	total_au_reviews: {
+		total: string
+		states: Array<{
+			key: string
+			total: string
+		}>
+	}
+	total_uk_reviews: {
+		total: string
+		states: Array<{
+			key: string
+			total: string
+		}>
+	}
+}
+
+const Stats = () => {
+	const [country, setCountry] = useState<string | null>(null)
+	// eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+	const {data, error} = useSWR<IStats>('/api/get-stats', fetcher)
+	if (error) return <div>failed to load</div>
+	if (!data) return <div>loading...</div>
+
+	const getComponent = () => {
+		switch (country) {
+			case 'CA':
+				return <StateStats states={data.total_ca_reviews.states} />
+			case 'US':
+				return <StateStats states={data.total_us_reviews.states} />
+			case 'UK':
+				return <StateStats states={data.total_uk_reviews.states} />
+			case 'AU':
+				return <StateStats states={data.total_au_reviews.states} />
+			default:
+				return <></>
+		}
+	}
+
+	return (
+		<div className="container flex w-full flex-wrap justify-center px-4 sm:px-6 lg:px-8">
+			<div className="mt-3 flex w-full flex-row justify-center gap-3">
+				<div className="flex flex-col p-6 text-center">
+					<dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+						Total Reviews
+					</dt>
+					<dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+						{data.total_reviews}
+					</dd>
+				</div>
+				<div
+					onClick={() => setCountry('CA')}
+					className={`flex cursor-pointer flex-col rounded-xl border p-6 text-center ${
+						country && country === 'CA' ? 'bg-gray-200' : ''
+					}`}
+				>
+					<dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+						Canadian Reviews
+					</dt>
+					<dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+						{data.total_ca_reviews.total}
+					</dd>
+				</div>
+				<div
+					onClick={() => setCountry('US')}
+					className={`flex cursor-pointer flex-col rounded-xl border p-6 text-center ${
+						country && country === 'US' ? 'bg-gray-200' : ''
+					}`}
+				>
+					<dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+						US Reviews
+					</dt>
+					<dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+						{data.total_us_reviews.total}
+					</dd>
+				</div>
+				<div
+					onClick={() => setCountry('UK')}
+					className={`flex cursor-pointer flex-col rounded-xl border p-6 text-center ${
+						country && country === 'UK' ? 'bg-gray-200' : ''
+					}`}
+				>
+					<dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+						UK Reviews
+					</dt>
+					<dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+						{data.total_uk_reviews.total}
+					</dd>
+				</div>
+				<div
+					onClick={() => setCountry('AU')}
+					className={`flex cursor-pointer flex-col rounded-xl border p-6 text-center ${
+						country && country === 'AU' ? 'bg-gray-200' : ''
+					}`}
+				>
+					<dt className="order-2 mt-2 text-lg font-medium leading-6 text-gray-500">
+						Australia Reviews
+					</dt>
+					<dd className="order-1 text-5xl font-bold tracking-tight text-indigo-600">
+						{data.total_au_reviews.total}
+					</dd>
+				</div>
+			</div>
+			{getComponent()}
+		</div>
+	)
+}
+
+export default Stats
--- frontend/pages/admin/[admin].tsx
+++ frontend/pages/admin/[admin].tsx
@@ -7,11 +7,13 @@ import MyInfo from '@/components/admin/sections/MyInfo'
 import {parseCookies} from 'nookies'
 import Link from 'next/link'
 import {NextSeo} from 'next-seo'
+import Stats from '@/components/admin/sections/Stats'
 
 const startingTabs = [
 	{name: 'Flagged Reviews', component: <FlaggedReviews />, current: true},
 	{name: 'Team Members', component: <TeamMembers />, current: false},
 	{name: 'My Info', component: <MyInfo />, current: false},
+	{name: 'Stats', component: <Stats />, current: false},
 ]
 
 function Admin(): JSX.Element {
@@ -32,11 +34,11 @@ function Admin(): JSX.Element {
 
 	if (noCookie) {
 		return (
-			<div className="w-full flex flex-col items-center gap-4">
+			<div className="flex w-full flex-col items-center gap-4">
 				<h1 className="text-center">Not Logged In</h1>
 				<Link
 					href="/login"
-					className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-teal-600 hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-teal-500"
+					className="ml-3 inline-flex justify-center rounded-md border border-transparent bg-teal-600 py-2 px-4 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
 				>
 					Go To Login
 				</Link>
@@ -44,7 +46,7 @@ function Admin(): JSX.Element {
 		)
 	}
 	return (
-		<div className="w-full flex flex-col items-center">
+		<div className="flex w-full flex-col items-center">
 			<NextSeo noindex={true} />
 			<Tabs
 				currentTab={currentTab}
