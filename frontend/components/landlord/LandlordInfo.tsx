7ef5b59a8e0c2db58b8250af8dff18dd67a02330
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-05-17 18:37:27 -0400

    Bug Fixes

--- frontend/components/landlord/LandlordInfo.tsx
+++ frontend/components/landlord/LandlordInfo.tsx
@@ -42,11 +42,10 @@ const LandlordInfo = ({name, average, total}: IProps) => {
 					other tenants
 				</p>
 
-				<Link
-					href="/create-review"
-					className="mt-2 inline-flex items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
-				>
-					Submit a review
+				<Link href="/create-review">
+					<p className="mt-2 inline-flex cursor-pointer items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2">
+						Submit a review
+					</p>
 				</Link>
 			</div>
 		</div>
