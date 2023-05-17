e51d5edd03b0db2816c288a8266e5fd7c8fc6c4a
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-05-17 08:01:19 -0400

    Bug Fix

--- frontend/components/landlord/LandlordInfo.tsx
+++ frontend/components/landlord/LandlordInfo.tsx
@@ -1,5 +1,6 @@
 import {classNames} from '@/util/helpers/helper-functions'
 import {StarIcon} from '@heroicons/react/solid'
+import Link from 'next/link'
 
 interface IProps {
 	name: string
@@ -41,12 +42,12 @@ const LandlordInfo = ({name, average, total}: IProps) => {
 					other tenants
 				</p>
 
-				<a
+				<Link
 					href="/create-review"
 					className="mt-2 inline-flex items-center rounded-md border border-transparent bg-teal-600 px-4 py-2 text-sm font-medium text-white shadow-sm hover:bg-teal-700 focus:outline-none focus:ring-2 focus:ring-teal-500 focus:ring-offset-2"
 				>
 					Submit a review
-				</a>
+				</Link>
 			</div>
 		</div>
 	)
