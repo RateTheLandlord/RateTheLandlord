de2df99ed2f9c52cb27456b87d07b4a0b768c374
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-06-06 08:02:02 -0400

    Update localization for About Page

--- frontend/components/about/aboutUs.test.tsx
+++ frontend/components/about/aboutUs.test.tsx
@@ -2,58 +2,25 @@
  * @jest-environment jsdom
  */
 import React from 'react'
-import {render, RenderResult} from '@testing-library/react'
+import {render, screen} from '@testing-library/react'
 import AboutUs from './aboutUs'
 
-// Define custom translations for testing
-const translations: Record<string, string> = {
-	'about.about-us.about': 'About Us',
-	'about.about-us.header': 'Header',
-	'about.about-us.p-1': 'Paragraph 1',
-	'about.about-us.p-2': 'Paragraph 2',
-	'about.about-us.p-3': 'Paragraph 3',
-	'about.about-us.p-4': 'Paragraph 4',
-}
-
-// Mock the useTranslation hook
 jest.mock('react-i18next', () => ({
-	useTranslation: () => {
-		return {
-			t: jest.fn((key: string) => translations[key] || key),
-		}
-	},
+	useTranslation: jest.fn().mockReturnValue({
+		t: jest.fn().mockImplementation((key) => {
+			if (key === 'about.about-us.about') {
+				return 'About Us'
+			} else if (key === 'about.about-us.info') {
+				return ['Info 1', 'Info 2', 'Info 3']
+			}
+		}),
+	}),
 }))
 
-describe('AboutUs', () => {
-	let renderResult: RenderResult
+test('renders about section with info items', () => {
+	render(<AboutUs />)
+	const aboutSection = screen.getByTestId('about-aboutus-1')
+	expect(aboutSection).toBeInTheDocument()
 
-	beforeEach(() => {
-		renderResult = render(<AboutUs />)
-	})
-
-	test('renders the component with translated content', () => {
-		const {getByTestId, getByText} = renderResult
-
-		// Check if the component renders correctly
-		const aboutUsElement = getByTestId('about-aboutus-1')
-		expect(aboutUsElement).toBeInTheDocument()
-
-		// Check if the translated text is displayed correctly
-		const headerElement = getByText('Header')
-		expect(headerElement).toBeInTheDocument()
-
-		const p1Element = getByText('Paragraph 1')
-		expect(p1Element).toBeInTheDocument()
-
-		const p2Element = getByText('Paragraph 2')
-		expect(p2Element).toBeInTheDocument()
-
-		const p3Element = getByText('Paragraph 3')
-		expect(p3Element).toBeInTheDocument()
-
-		const p4Element = getByText('Paragraph 4')
-		expect(p4Element).toBeInTheDocument()
-	})
-
-	// Add more test cases as needed
+	expect(screen.getByText('About Us')).toBeInTheDocument()
 })
--- frontend/components/about/moderation.test.tsx
+++ frontend/components/about/moderation.test.tsx
@@ -2,26 +2,33 @@
  * @jest-environment jsdom
  */
 
+import React from 'react'
 import {render, screen} from '@testing-library/react'
 import Moderation from './moderation'
 
-describe('Moderation', () => {
-	it('renders moderation paragraphs correctly', () => {
-		render(<Moderation />)
+jest.mock('react-i18next', () => ({
+	useTranslation: jest.fn().mockReturnValue({
+		t: jest.fn().mockImplementation((key) => {
+			if (key === 'about.moderation.moderation') {
+				return 'Moderation Policy'
+			} else if (key === 'about.moderation.info') {
+				return ['Info paragraph 1', 'Info paragraph 2']
+			}
+		}),
+	}),
+}))
 
-		const paragraphs = screen.getAllByRole('paragraph')
-		const expectedParagraphs = [
-			'Tenants visit Rate The Landlord to find information on prospective landlords based on reviews from their previous tenants. We will carefully moderate the submitted reviews to ensure they are relevant, appropriate, and respect the privacy of both parties.',
-			'We strictly prohibit the posting of threats, hate speech, lewd or discriminatory language.',
-			"At Rate The Landlord, privacy is important. A landlord's name is used in reviews because they operate a business under that name. However, we do not permit the posting of addresses, phone numbers, or any personal information related to the landlord or other parties involved.",
-			'Any reviews found in violation of this policy will be amended or removed at our discretion. We remain neutral and will not engage in factual disputes regarding the content of the reviews.',
-			// Update with the content of other paragraphs...
-		]
+test('renders moderation section with title and info paragraphs', () => {
+	render(<Moderation />)
+	const moderationSection = screen.getByTestId('about-moderation-1')
+	expect(moderationSection).toBeInTheDocument()
 
-		expect(paragraphs).toHaveLength(4) // Assuming there are 4 paragraphs
+	expect(screen.getByText('Moderation Policy')).toBeInTheDocument()
 
-		paragraphs.forEach((paragraph, index) => {
-			expect(paragraph).toHaveTextContent(expectedParagraphs[index])
-		})
-	})
+	const infoParagraphs = moderationSection.querySelectorAll(
+		"p[role='paragraph']",
+	)
+	expect(infoParagraphs).toHaveLength(2)
+	expect(infoParagraphs[0]).toHaveTextContent('Info paragraph 1')
+	expect(infoParagraphs[1]).toHaveTextContent('Info paragraph 2')
 })
