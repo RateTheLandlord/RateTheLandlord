fe421c722542d3512a0c2ea2ec914bff0b03cdd8
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-06-07 07:38:08 -0400

    Refactoring

--- frontend/components/ui/link-button-light-lg.tsx
+++ frontend/components/ui/link-button-light-lg.tsx
@@ -1,17 +1,18 @@
 import Link from 'next/link'
 import React from 'react'
 
-export default function LinkButtonLightLG({
-	children,
-	href,
-}: {
+interface IProps {
 	children: string
 	href: string
-}) {
+}
+export default function LinkButtonLightLG({children, href}: IProps) {
 	return (
-		<div className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3" data-testid="home-hero-read-btn-1">
+		<div
+			className="mt-3 rounded-md shadow sm:mt-0 sm:ml-3"
+			data-testid="home-hero-read-btn-1"
+		>
 			<Link href={href}>
-				<a className="w-full flex items-center justify-center px-8 py-3 border border-teal-600 text-base font-medium rounded-md text-teal-600 bg-white hover:bg-gray-50 md:py-4 md:text-lg md:px-10">
+				<a className="flex w-full items-center justify-center rounded-md border border-teal-600 bg-white px-8 py-3 text-base font-medium text-teal-600 hover:bg-gray-50 md:py-4 md:px-10 md:text-lg">
 					{children}
 				</a>
 			</Link>
