d5295db93450ab94fd232dc840618ac550752c4c
Kellen Wiltshire <webdevelopment@kellenwiltshire.com>
2023-06-18 11:59:21 -0400

    Update analytics link (#306)
    Co-authored-by: RateTheLandlordAdmin <r8thelandlord@gmail.com>

--- frontend/pages/_document.tsx
+++ frontend/pages/_document.tsx
@@ -1,4 +1,5 @@
-import Document, {Head, Html, Main, NextScript} from 'next/document'
+import Document, { Head, Html, Main, NextScript } from "next/document";
+
 const isProd = process.env.NODE_ENV === 'production'
 
 export default class MyDocument extends Document {
@@ -17,11 +18,7 @@ export default class MyDocument extends Document {
 								src="https://static.cloudflareinsights.com/beacon.min.js"
 								data-cf-beacon='{"token": "8cf4d134ee8a33518b72e78bc9e1eaa8", "spa": true}'
 							/>
-							<script
-								async
-								src="https://analytics.umami.is/script.js"
-								data-website-id="7feb6f2e-e933-4615-a403-d2be156c61e2"
-							></script>
+							<script async src="http://159.203.4.57:3500/script.js" data-website-id="035d8dc9-e6d9-40b7-852b-e6a750c46a09"></script>
 						</>
 					)}
 				</body>
