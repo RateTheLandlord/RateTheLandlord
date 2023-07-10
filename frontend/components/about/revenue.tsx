105073e3b993fca97e9901cccb42e417c89a1081
RateTheLandlordAdmin <r8thelandlord@gmail.com>
2023-07-09 20:20:56 -0400

    Add Test

--- frontend/components/about/revenue.tsx
+++ frontend/components/about/revenue.tsx
@@ -3,7 +3,7 @@ import {useTranslation} from 'react-i18next'
 const Revenue = () => {
 	const {t} = useTranslation('about')
 	return (
-		<div data-testid="about-aboutus-1" className="w-full bg-white">
+		<div data-testid="about-revenue-1" className="w-full bg-white">
 			<div className="mx-auto max-w-7xl px-6 text-lg lg:px-8">
 				<h3 className="mt-2 block text-center text-xl font-bold leading-8 tracking-tight text-gray-900 sm:text-2xl">
 					{t('about.revenue.title')}
