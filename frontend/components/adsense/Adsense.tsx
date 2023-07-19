e29a450bf5255b6b735c704d902903ea1e9e55f2
Kellen Wiltshire <webdevelopment@kellenwiltshire.com>
2023-07-19 09:47:15 -0400

    Adsense fix (#348)
    * Update classes for Adsense
    
    * Reinstate prod check
    
    * Update Adsense again

--- frontend/components/adsense/Adsense.tsx
+++ frontend/components/adsense/Adsense.tsx
@@ -1,14 +1,19 @@
+import {useEffect} from 'react'
+
 const isProd = process.env.NODE_ENV === 'production'
 
-const Adsense = () => {
+const AdsComponent = () => {
+	useEffect(() => {
+		// Only load the Google AdSense script in production to comply with AdSense policies
+		if (isProd) {
+			// @ts-ignore
+			;(window.adsbygoogle = window.adsbygoogle || []).push({})
+		}
+	}, [])
+
 	if (isProd) {
 		return (
 			<div className="mx-auto max-w-2xl px-4 sm:px-6 lg:max-w-7xl lg:px-8">
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1233437669445756"
-					crossOrigin="anonymous"
-				></script>
 				<ins
 					className="adsbygoogle h-[100px] md:h-[90px]"
 					style={{display: 'block'}}
@@ -17,10 +22,12 @@ const Adsense = () => {
 					data-ad-format="auto"
 					data-full-width-responsive="true"
 				></ins>
-				<script>(adsbygoogle = window.adsbygoogle || []).push({});</script>
 			</div>
 		)
-	} else return null
+	} else {
+		// For development environment, don't render the ad unit
+		return null
+	}
 }
 
-export default Adsense
+export default AdsComponent
